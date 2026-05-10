0156636d8e257bbc90aa8084b00f7f1481a73e76
naman159 <namanranawat159@gmail.com>
2026-05-11 01:27:05 +0530

    feat: wire Locations, Objects, Factions routes and sidebar links

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,13 @@ import CharacterDetail from "./routes/CharacterDetail";
 import CharacterList from "./routes/CharacterList";
 import Chapters from "./routes/Chapters";
 import Continuity from "./routes/Continuity";
+import FactionDetail from "./routes/FactionDetail";
+import Factions from "./routes/Factions";
+import LocationDetail from "./routes/LocationDetail";
+import Locations from "./routes/Locations";
 import Novels from "./routes/Novels";
+import ObjectDetail from "./routes/ObjectDetail";
+import Objects from "./routes/Objects";
 import Process from "./routes/Process";
 import Relationships from "./routes/Relationships";
 import SharedDynamics from "./routes/SharedDynamics";
@@ -31,6 +37,12 @@ export default function App() {
           <Route path="/novels/:novelId/continuity" element={<Layout><Continuity /></Layout>} />
           <Route path="/novels/:novelId/relationships" element={<Layout><Relationships /></Layout>} />
           <Route path="/novels/:novelId/dynamics" element={<Layout><SharedDynamics /></Layout>} />
+          <Route path="/novels/:novelId/locations" element={<Layout><Locations /></Layout>} />
+          <Route path="/novels/:novelId/locations/:locationId" element={<Layout><LocationDetail /></Layout>} />
+          <Route path="/novels/:novelId/objects" element={<Layout><Objects /></Layout>} />
+          <Route path="/novels/:novelId/objects/:objectId" element={<Layout><ObjectDetail /></Layout>} />
+          <Route path="/novels/:novelId/factions" element={<Layout><Factions /></Layout>} />
+          <Route path="/novels/:novelId/factions/:factionId" element={<Layout><FactionDetail /></Layout>} />
           <Route path="/novels/:novelId/process" element={<Layout><Process /></Layout>} />
         </Routes>
       </Router>
